e12dbb0324f3289e60ecb66bd62139d086324f79
Tyler <sudobash@126.com>
2018-04-12 15:51:50 +0800

    new question solution

--- 0002-add-two-numbers.go
+++ 0002-add-two-numbers.go
@@ -1,19 +1,89 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type ListNode struct {
-	v uint
+	v int
 	next *ListNode
 }
 
-func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+func main(){
+	l1 := createListNode([]int{2,4,3})
+	l2 := createListNode([]int{5,6,4})
 
+	res := AddTwoNumbers(l1,l2)
+	res.Print()
 }
 
-func createListNode(n uint){
-	var l *ListNode
-	for n>0{
-		if l.v
-		l.v = n%10
-		n = n/10
+func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var res, newNode *ListNode
+	var over int
+	for{
+		tmpSum := over
+		over = 0
+		if l1 !=nil{
+			tmpSum += l1.v
+			l1 = l1.next
+		}
+		if l2 !=nil{
+			tmpSum += l2.v
+			l2 = l2.next
+		}
+		if tmpSum > 9{
+			tmpSum -= 10
+			over = 1
+		}
+
+		if res == nil{
+			newNode = &ListNode{
+				v:tmpSum,
+			}
+			res = newNode //res指向第一个node
+		}else{
+			newNode.next = &ListNode{
+				v:tmpSum,
+			}
+			newNode = newNode.next //new总指向最新的node
+		}
+		if l1 == nil && l2 == nil {
+			if over > 0{
+				newNode.next = &ListNode{
+					v:over,
+				}
+			}
+			break
+		}
 	}
+	return res
+}
+func (l *ListNode) Print() {
+	var v string
+	for {
+		v = strconv.Itoa(l.v) + v
+		if l.next==nil {
+			break
+		}
+		l = l.next
+	}
+	fmt.Println(v)
+}
+func createListNode(num []int) *ListNode{
+	if len(num)==0{
+		return &ListNode{}
+	}
+	var pre *ListNode
+	for _,v := range num{
+		tmp := &ListNode{
+			v:v,
+		}
+		if pre!=nil{
+			tmp.next = pre
+		}
+		pre = tmp
+	}
+
+	return  pre
 }
